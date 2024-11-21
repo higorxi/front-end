f2bd72104f353a716d2d3d5b07b2cdde34282162
higorxi <higorgiovane7@gmail.com>
2024-11-21 01:07:40 -0300

    fix bug in footer

--- src/components/document/DocumentTableFooter.tsx
+++ src/components/document/DocumentTableFooter.tsx
@@ -1,35 +1,51 @@
 import { Table, TableFooter, TableRow, TableCell } from "../ui/table";
 
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function DocumentTableFooter({data}: {data : any}) {
+export function DocumentTableFooter({ data }: { data: any }) {
+
+  const formatCurrency = (value: number) => {
+    return `R$ ${value.toFixed(2).replace('.', ',')}`;
+  };
+
   return (
     <Table className="w-full">
       <TableFooter className="bg-gray-100">
         <TableRow className="flex justify-between mx-8">
-          <TableCell className="mb-4 w-1/6">
+          <TableCell className="w-1/6">
             <span className="size-3">Total</span>
-            <span className="block font-normal size-3.5">{data.totalDocuments}</span>
+            <div className="flex flex-col items-start">
+              <span className="font-normal size-3.5">
+                {data.totalDocuments}
+              </span>
+            </div>
           </TableCell>
           <TableCell className="w-1/6">
             <span className="size-3">n de emitentes</span>
-            <span className="block font-normal size-3.5">{data.uniqueEmitentes}</span>
+            <div className="flex flex-col items-start">
+              <span className="font-normal size-3.5">
+                {data.uniqueEmitentes}
+              </span>
+            </div>
           </TableCell>
           <TableCell className="w-1/6">
             <span className="size-3">Total de tributos</span>
-            <span className="block font-normal size-3.5">R$ {data.totalTributos}</span>
+            <div className="flex flex-col items-start">
+              <span className="font-normal size-3.5" style={{ whiteSpace: 'nowrap' }}>
+                {formatCurrency(data.totalTributos)}
+              </span>
+            </div>
           </TableCell>
           <TableCell className="w-1/6">
             <span className="size-3">Total valor liquido</span>
-            <span className="block font-normal size-3.5">R$ {data.totalValorLiquido}</span>
+            <div className="flex flex-col items-start">
+              <span className="font-normal size-3.5" style={{ whiteSpace: 'nowrap' }}>
+                {formatCurrency(data.totalValorLiquido)}
+              </span>
+            </div>
           </TableCell>
           <TableCell className="w-1/6">
-            <span className="size-3"></span>
-            <span className="block font-normal size-3.5"></span>
           </TableCell>
           <TableCell className="w-1/6">
-            <span className="size-3"></span>
-            <span className="block font-normal size-3.5"></span>
           </TableCell>
         </TableRow>
       </TableFooter>
